49f8efaea597cf330bffe5cfd32c24eb132b2a47
sblatz <sblatz@purdue.edu>
2018-01-31 11:57:46 -0500

    Initial Project Setup

---
+++ Get Rec'd/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Get Rec'd
+//
+//  Created by Sawyer Blatz on 1/31/18.
+//  Copyright © 2018 CS 407. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
